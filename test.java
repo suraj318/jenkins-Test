9988606228a37eee1d9ec80a20ef7b89a851d10c
SurajGm <surajmandhane40@gmail.com>
2025-03-11 11:08:55 +0530

    test.java file added

---
+++ test.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello, This is Jenkins Test");
+
+        System.out.println("\nTest Class\n");
+
+        System.out.println(new Date());
+
+        System.out.println("Added Somethings");
+    }
+    
+
+}
